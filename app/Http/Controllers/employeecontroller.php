97075f88115a65e236a58d1f1dc560276c6085dc
romielmanabat <pandiojames18@gmail.com>
2025-05-30 16:31:17 +0800

    added validation

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -20,14 +20,28 @@ class employeecontroller extends Controller
     }
 
     public function store(Request $request){
-       
+       $request->validate([
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
+       ]);
 
         employee::create($request->all());
         return view('employee.create');
     }
 
     public function update(Request $request, int $id){
-       
+       $request->validate([
+        'fname' => 'required|max:255|string',
+        'lname' => 'required|max:255|string',
+        'midname' => 'required|max:255|string',
+        'age' => 'required|integer',
+        'address' => 'required|max:255|string',
+        'zip' => 'required|integer',
+       ]);
 
         employee::findOrFail($id)->update($request->all());
         return redirect()->back()->with('status', 'Employee Updated');
