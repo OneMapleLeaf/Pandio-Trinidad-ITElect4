7776e354f46d423bb2086e7c4442a9f863d204d0
romielmanabat <pandiojames18@gmail.com>
2025-05-30 16:06:48 +0800

    fixed store

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -20,27 +20,12 @@ class employeecontroller extends Controller
     }
 
     public function store(Request $request){
-        $request->validate([
-            'fname' => 'required|max:255|string',
-            'lname' => 'required|max:255|string',
-            'midname' => 'required|max:255|string',
-            'age' => 'required|integer',
-            'adress' => 'required|max:255|string',
-            'zip' => 'required|integer'
-        ]);
+       
 
         employee::create($request->all());
         return view('employee.create');
     }
 
-    // employee::create($request->all());
-    // return view ('employee.create');
-    // }
-
-
-        
-            // employee::findOrFail($id)->update($request->all());
-            // return redirect ()->back()->with('status','Employee Updated Successfully!');
-            // }
+   
 
 }
