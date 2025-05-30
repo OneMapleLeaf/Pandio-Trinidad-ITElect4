6308ce2582b08a32c1fcf463223d7445c22438ee
Decimmm <rommeryumang@gmail.com>
2025-05-30 16:21:31 +0800

    Update, delete and edit controller created

--- app/Http/Controllers/employeecontroller.php
+++ app/Http/Controllers/employeecontroller.php
@@ -26,6 +26,27 @@ class employeecontroller extends Controller
         return view('employee.create');
     }
 
-   
+    public function update(Request $request, int $id){
+       
+
+        employee::findOrFail($id)->update($request->all());
+        return redirect()->back()->with('status', 'Employee Updated');
+    }
+
+    public function edit(int $id)
+    {   
+        $employees = employee::find($id);
+
+        return view ('employee.edit', compact('employees'));
+    }
+
+    public function destroy(int $id){
+       
+
+        employee::findOrFail($id)->delete();
+        return redirect()->back()->with('status', 'Employee Deleted');
+    }
+
+
 
 }
--- resources/views/employee/index.blade.php
+++ resources/views/employee/index.blade.php
@@ -59,8 +59,8 @@
                                         <td>{{ $items->address }}</td>
                                         <td>{{ $items->zip }}</td>
                                         <td> 
-                                                <span class="badge bg-success"><a class="btn btn-success mx-3"><h5>Edit</h5></a></span>
-                                                <span class="badge bg-danger"><a class="btn btn-danger mx-3"><h5>Delete</h5></a></span>
+                                                <span class="badge bg-success"><a class="btn btn-success mx-3" href="{{route('employee.edit', $items->id)}}"><h5>Edit</h5></a></span>
+                                                <span class="badge bg-danger"><a class="btn btn-danger mx-3" href="{{route('employee.delete', $items->id)}}"><h5>Delete</h5></a></span>
                                         </td>       
                                     </tr>
                                     @endforeach
